a92ef757c2a68c89834fb95f84335b2d1e561e70
Yes-ID33 <148925837+Yes-ID33@users.noreply.github.com>
2025-03-26 18:36:48 -0500

    Express funciona

---
+++ src/routes/routes.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const router = express.Router();
+
+// Obtener el directorio actual
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Ruta para la página principal
+router.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '../views', 'index.html'));
+});
+
+// Ruta para la página de inicio de sesión
+router.get('/login.html', (req, res) => {
+    res.sendFile(join(__dirname, '../views', 'login.html'));
+});
+
+// Ruta para la página de registro
+router.get('/register.html', (req, res) => {
+    res.sendFile(join(__dirname, '../views', 'register.html'));
+});
+
+// Ruta para otras páginas
+router.get('/servicios.html', (req, res) => {
+    res.sendFile(join(__dirname, '../views', 'servicios.html'));
+});
+
+export default router;
